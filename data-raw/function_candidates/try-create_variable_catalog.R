bc7b5dc0ee8d3b6bb8de84c0ad096a967bcd8392
Daniel Antal <antaldaniel@gmail.com>
2025-01-17 17:38:07 +0100

    function candidate to modernize variable-level metadata collection

--- data-raw/function_candidates/try-create_variable_catalog.R
+++ data-raw/function_candidates/try-create_variable_catalog.R
@@ -1,25 +1,12 @@
+gesis_files <- dir(here::here("data-raw", "gesis"))
+gesis_files <- gesis_files[grepl(x = gesis_files, pattern = ".sav")]
+dataset_id <- substr(gesis_files,1, 16)
+
 gesis_files <- tibble::tibble(
-  survey_file = file.path(
-    "data-raw/gesis",
-    c(
-      "ZA4529_v3-0-1.sav",
-      "ZA5688_v6-0-0.sav",
-      "ZA7780_v2-0-0.sav",
-      "ZA8904_v1-0-0.sav",
-      "ZA8905_v1-0-0.sav"
-    )
-  ),
-  
-  dataset_id = c(
-    "ZA4529",
-    "ZA5688",
-    "ZA7780",
-    "ZA8904",
-    "ZA8905"
-  )
+  survey_file = here::here("data-raw", "gesis", gesis_files),
+  dataset_id = dataset_id
 )
 
-
-
-survey_catalogue <- create_variable_catalog(survey_files = gesis_files$survey_file,
-               dataset_id = gesis_files$dataset_id)
+survey_catalogue <- create_variable_catalog(
+  survey_files = gesis_files$survey_file,
+  dataset_id = gesis_files$dataset_id)
