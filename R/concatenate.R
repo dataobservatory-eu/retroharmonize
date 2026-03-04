6ca518fa20c49f2d38ac4bb2fd0eac0d1440c59b
Daniel Antal <antaldaniel@gmail.com>
2026-03-04 17:01:19 +0100

    Preserve the structure of vectors in SPSS files

--- R/concatenate.R
+++ R/concatenate.R
@@ -37,32 +37,46 @@
 #' @export
 
 concatenate <- function(x, y) {
-  is.labelled_spss <- function(x) {
-    inherits(x, "haven_labelled_spss")
-  }
-
+  
   validate_concatenate(x, y)
-
+  
   label_x <- attr(x, "label")
   label_y <- attr(y, "label")
-
-  if (sum(is.null(label_x), is.null(label_y)) == 1) {
+  
+  if (xor(is.null(label_x), is.null(label_y))) {
+    
     if (is.null(label_x)) {
-      attr(x, "label") <- attr(y, "label")
-      warning("The variable label of y <", label_y, "> will be used as variable label.")
+      
+      attr(x, "label") <- label_y
+      
+      warning(
+        "The variable label of y <",
+        label_y,
+        "> will be used as variable label."
+      )
+      
     } else {
-      warning("The variable label of x <", label_x, "> will be used as variable label.")
+      
+      warning(
+        "The variable label of x <",
+        label_x,
+        "> will be used as variable label."
+      )
     }
-  } else {
-    if (!all.equal(label_x, label_y)) {
-      warning("The variable labels are not the same, <", label_x, "> of x will be used.")
-    }
+    
+  } else if (!isTRUE(all.equal(label_x, label_y))) {
+    
+    warning(
+      "The variable labels are not the same, <",
+      label_x,
+      "> of x will be used."
+    )
   }
-
-  z <- joining_attributes(x, y)
-  z
+  
+  joining_attributes(x, y)
 }
 
+#' @keywords internal
 validate_concatenate <- function(x, y) {
   if (!all(c(haven::is.labelled(x), haven::is.labelled(y)))) {
     stop("Both arguments must be labelled")
@@ -115,35 +129,56 @@ validate_concatenate <- function(x, y) {
   }
 }
 
+#' @keywords internal
 joining_attributes <- function(x, y) {
+  
   s1 <- attributes(x)
   s2 <- attributes(y)
-  same_attributes <- intersect(names(s1), names(s2))
-  compare_attributes <- vapply(same_attributes, function(x) setequal(s1[[x]], s2[[x]]), logical(1))
-  matching_arguments <- names(compare_attributes[compare_attributes == TRUE])
-
-
+  
+  same_attributes <- intersect(
+    names(s1),
+    names(s2)
+  )
+  
+  compare_attributes <- vapply(
+    same_attributes,
+    function(attr_name) {
+      isTRUE(
+        setequal(
+          s1[[attr_name]],
+          s2[[attr_name]]
+        )
+      )
+    },
+    logical(1)
+  )
+  
+  matching_arguments <- names(
+    compare_attributes[compare_attributes]
+  )
+  
   c_vector <- structure(
     vctrs::vec_c(
       vctrs::vec_data(x),
       vctrs::vec_data(y)
     ),
-    id = c(attr(x, "id"), attr(y, "id"))
+    id = c(
+      attr(x, "id"),
+      attr(y, "id")
+    )
   )
-  c_vector
-
-
+  
   for (i in matching_arguments) {
     attr(c_vector, i) <- attr(x, i)
   }
-
+  
   for (x_attr in setdiff(names(s1), names(s2))) {
     attr(c_vector, x_attr) <- attr(x, x_attr)
   }
-
+  
   for (y_attr in setdiff(names(s2), names(s1))) {
     attr(c_vector, y_attr) <- attr(y, y_attr)
   }
-
+  
   c_vector
 }
