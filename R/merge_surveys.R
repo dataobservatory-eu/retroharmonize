5d99ff0fa8358d11781c6f77c0965927948f56c3
Daniel Antal <antaldaniel@gmail.com>
2026-03-04 17:28:05 +0100

    Code readability

--- R/merge_surveys.R
+++ R/merge_surveys.R
@@ -1,48 +1,62 @@
-#' @title Merge surveys
+#' Merge and harmonize surveys
+#'
+#' Harmonize variable names, labels, and identifiers across multiple
+#' surveys using a metadata crosswalk table.
 #'
 #' @description
-#' Merge a list of surveys into a list with harmonized variable names,
-#' variable labels, and survey identifiers.
+#' `merge_surveys()` applies a harmonization specification to a list
+#' of survey objects and returns harmonized survey datasets with
+#' aligned variable names and metadata.
 #'
 #' @details
-#' Prior to version 0.2.0 this function was called \code{merge_waves()},
-#' reflecting the terminology used in Eurobarometer surveys.
+#' Prior to version 0.2.0 this function was called `merge_waves()`,
+#' reflecting terminology commonly used in Eurobarometer surveys.
 #'
-#' @param survey_list
-#' A list of surveys.
+#' The harmonization table supplied in `var_harmonization`
+#' typically originates from [metadata_create()] and contains
+#' mappings between original and harmonized variable names.
 #'
-#' @param var_harmonization
-#' A metadata table describing how variables should be harmonized.
-#' It must contain at least the columns
-#' \code{filename}, \code{var_name_orig}, \code{var_name_target},
-#' and \code{var_label}.
+#' @param survey_list A list of survey objects.
+#'
+#' @param var_harmonization A metadata table describing the
+#'   harmonization rules. The table must contain at least:
+#'
+#'   - `filename`
+#'   - `var_name_orig`
+#'   - `var_name_target`
+#'   - `var_label`
 #'
 #' @return
-#' A list of surveys with harmonized variable names and labels.
+#' A list of harmonized survey objects with standardized variable names
+#' and variable labels.
 #'
 #' @family survey harmonization functions
 #'
-#' @seealso metadata_create
-#'
-#' @importFrom rlang set_names .data
-#' @importFrom dplyr select mutate filter across any_of
-#' @importFrom haven is.labelled
-#' @importFrom tidyselect all_of
+#' @seealso
+#' [metadata_create()]
 #'
 #' @examples
 #' \donttest{
-#' examples_dir <- system.file("examples", package = "retroharmonize")
-#' survey_files <- dir(examples_dir, pattern = "\\.rds$", full.names = TRUE)
 #'
-#' example_surveys <- read_surveys(
-#'   survey_files,
-#'   save_to_rds = FALSE
+#' examples_dir <- system.file(
+#'   "examples",
+#'   package = "retroharmonize"
 #' )
 #'
-#' # Create metadata from surveys
-#' metadata <- metadata_create(survey_list = example_surveys)
+#' survey_files <- dir(
+#'   examples_dir,
+#'   pattern = "\\.rds$",
+#'   full.names = TRUE
+#' )
 #'
-#' # Select and harmonize a subset of variables
+#' example_surveys <- read_surveys(
+#'   survey_files
+#' )
+#'
+#' metadata <- metadata_create(
+#'   survey_list = example_surveys
+#' )
+#'
 #' to_harmonize <- metadata %>%
 #'   dplyr::filter(
 #'     var_name_orig %in% c("rowid", "w1") |
@@ -52,7 +66,8 @@
 #'     var_label = var_label_normalize(var_label_orig),
 #'     var_name_target = val_label_normalize(var_label),
 #'     var_name_target = ifelse(
-#'       .data$var_name_orig %in% c("rowid", "w1", "wex"),
+#'       .data$var_name_orig %in%
+#'         c("rowid", "w1", "wex"),
 #'       .data$var_name_orig,
 #'       .data$var_name_target
 #'     )
@@ -66,16 +81,22 @@
 #' merged_surveys[[1]]
 #' }
 #'
+#' @importFrom dplyr across any_of filter mutate select
+#' @importFrom haven is.labelled
+#' @importFrom rlang .data set_names
+#' @importFrom tidyselect all_of
+#'
 #' @export
-
 
 merge_surveys <- function(survey_list, var_harmonization) {
   validate_survey_list(survey_list)
 
-  if (any(!c("filename", "var_name_orig", "var_name_target", "var_label") %in% names(var_harmonization))) {
+  if (any(!c("filename", "var_name_orig", "var_name_target", "var_label") %in% 
+          names(var_harmonization))) {
     stop(
       "var_harmonization must contain ",
-      paste(c("filename", "var_name_orig", "var_name_target", "var_label"), collapse = ", "),
+      paste(c("filename", "var_name_orig", "var_name_target", "var_label"), 
+            collapse = ", "),
       "."
     )
   }
@@ -122,11 +143,26 @@ merge_surveys <- function(survey_list, var_harmonization) {
   lapply(survey_list, fn_merge)
 }
 
-#' @title merge_waves
-#' @rdname merge_surveys
-#' @param waves Deprecated.
+#' Deprecated wrapper for `merge_surveys()`
+#'
+#' `merge_waves()` has been renamed to [merge_surveys()]
+#' for more general survey harmonization workflows.
+#'
+#' @inheritParams merge_surveys
+#' @param waves Deprecated alias for `survey_list`.
+#'
+#' @return
+#' A list of harmonized survey objects.
+#'
+#' @seealso
+#' [merge_surveys()]
+#'
+#' @family survey harmonization functions
+#'
 #' @export
+
 merge_waves <- function(waves, var_harmonization) {
+  
   merge_surveys(
     survey_list = waves,
     var_harmonization = var_harmonization
