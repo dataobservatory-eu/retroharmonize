d989218cbeb650edaa7affe17443c9827e8524a6
Daniel Antal <antaldaniel@gmail.com>
2026-03-04 15:33:31 +0100

    Improve documentation

--- R/pull_survey.R
+++ R/pull_survey.R
@@ -1,19 +1,35 @@
-#' @title Pull a survey from a survey list
+#' Retrieve a survey from a survey list
 #'
-#' @description Pull a survey by survey code or id.
+#' Extract a single survey object from a list of surveys using either
+#' its survey identifier or source file name.
 #'
-#' @param survey_list A list of surveys
-#' @param id The id of the requested survey. If \code{NULL} use
-#' \code{filename}
-#' @param filename The filename of the requested survey.
-#' @return A single survey identified by \code{id} or \code{filename}.
-#' @export
+#' @param survey_list A list of `survey` objects.
+#' @param id Optional survey identifier stored in the `"id"` attribute.
+#' @param filename Optional source file name stored in the `"filename"`
+#'   attribute.
+#'
+#' @return
+#' A single `survey` object.
+#'
+#' @details
+#' Either `id` or `filename` must be supplied.
+#'
+#' The function throws an error if:
+#'
+#' - neither identifier is provided,
+#' - the requested survey is not found,
+#' - or multiple surveys match the query.
+#'
 #' @family import functions
+#'
 #' @examples
-#' examples_dir <- system.file("examples", package = "retroharmonize")
+#' examples_dir <- system.file(
+#'   "examples",
+#'   package = "retroharmonize"
+#' )
 #'
 #' my_rds_files <- dir(examples_dir)[grepl(
-#'   ".rds",
+#'   "\\.rds$",
 #'   dir(examples_dir)
 #' )]
 #'
@@ -21,7 +37,13 @@
 #'   file.path(examples_dir, my_rds_files)
 #' )
 #'
-#' pull_survey(example_surveys, id = "ZA5913")
+#' pull_survey(
+#'   example_surveys,
+#'   id = "ZA5913"
+#' )
+#'
+#' @export
+
 pull_survey <- function(survey_list, id = NULL, filename = NULL) {
   if (is.null(id) && is.null(filename)) {
     stop("Either the id or the filename must be given")
