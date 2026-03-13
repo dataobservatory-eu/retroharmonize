98dba93dd00e01191a9f2a053f23e91513314cee
Daniel Antal <antaldaniel@gmail.com>
2026-03-13 17:42:40 +0100

    s3 class unit tests cleanup

--- tests/testthat/test-labelled_spss_survey-print.R
+++ tests/testthat/test-labelled_spss_survey-print.R
@@ -1,16 +1,15 @@
 test_that("vec_ptype_full returns readable type", {
-  
   x <- labelled_spss_survey(
     x = 1:3,
     labels = c(Yes = 1),
     id = "survey1"
   )
-  
+
   out <- vctrs::vec_ptype_full(x)
-  
+
   expect_type(out, "character")
   expect_length(out, 1)
-  
+
   expect_match(
     out,
     "labelled_spss_survey"
@@ -19,37 +18,34 @@ test_that("vec_ptype_full returns readable type", {
 
 
 test_that("vec_ptype_abbr returns compact abbreviation", {
-  
   x <- labelled_spss_survey(
     x = 1:3,
     labels = c(Yes = 1),
     id = "survey1"
   )
-  
+
   out <- vec_ptype_abbr(x)
-  
+
   expect_equal(out, "retroh_int")
 })
 
 
 test_that("format returns character vector", {
-  
   x <- labelled_spss_survey(
     x = c(1, 2, 9),
     labels = c(Yes = 1, No = 2),
     na_values = 9,
     id = "survey1"
   )
-  
+
   out <- format(x)
-  
+
   expect_type(out, "character")
   expect_length(out, 3)
 })
 
 
 test_that("print method works", {
-  
   x <- labelled_spss_survey(
     x = c(1, 2, 9),
     labels = c(Yes = 1, No = 2),
@@ -57,7 +53,7 @@ test_that("print method works", {
     label = "Question text",
     id = "survey1"
   )
-  
+
   expect_snapshot_output(
     print(x)
   )
@@ -65,14 +61,13 @@ test_that("print method works", {
 
 
 test_that("pillar shaft works", {
-  
   x <- labelled_spss_survey(
     x = 1:10,
     labels = c(Good = 1, Bad = 8),
     na_values = c(9, 10),
     id = "survey1"
   )
-  
+
   expect_snapshot_output(
     pillar::pillar_shaft(tibble::tibble(v1 = x))
   )
@@ -80,14 +75,13 @@ test_that("pillar shaft works", {
 
 
 test_that("pillar display works", {
-  
   x <- labelled_spss_survey(
     x = 1:10,
     labels = c(Good = 1, Bad = 8),
     na_values = c(9, 10),
     id = "survey1"
   )
-  
+
   expect_snapshot_output(
     pillar::pillar(x)
   )
--- tests/testthat/test-labelled_spss_survey-vctrs.R
+++ tests/testthat/test-labelled_spss_survey-vctrs.R
@@ -5,10 +5,10 @@ test_that("ptype2 with base numeric types returns double prototype", {
     na_values = 9,
     id = "survey1"
   )
-  
+
   expect_equal(vctrs::vec_ptype2(x, double()), double())
   expect_equal(vctrs::vec_ptype2(double(), x), double())
-  
+
   expect_equal(vctrs::vec_ptype2(x, integer()), double())
   expect_equal(vctrs::vec_ptype2(integer(), x), double())
 })
@@ -20,19 +20,18 @@ test_that("cast to base numeric types drops labelled_spss_survey class", {
     na_values = 9,
     id = "survey1"
   )
-  
+
   out_double <- vctrs::vec_cast(x, double())
   out_integer <- vctrs::vec_cast(x, integer())
-  
+
   expect_equal(out_double, c(1, 0, 9))
   expect_type(out_double, "double")
   expect_false(inherits(out_double, "retroharmonize_labelled_spss_survey"))
-  
+
   expect_equal(out_integer, c(1L, 0L, 9L))
   expect_type(out_integer, "integer")
   expect_false(inherits(out_integer, "retroharmonize_labelled_spss_survey"))
 })
-
 
 
 test_that("compatible labelled survey vectors have labelled survey prototype", {
@@ -44,7 +43,7 @@ test_that("compatible labelled survey vectors have labelled survey prototype", {
     id = "survey1",
     name_orig = "q1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(0, 1, 9),
     labels = c(yes = 1, no = 0, inap = 9),
@@ -53,9 +52,9 @@ test_that("compatible labelled survey vectors have labelled survey prototype", {
     id = "survey2",
     name_orig = "q1"
   )
-  
+
   ptype <- vctrs::vec_ptype2(x, y)
-  
+
   expect_s3_class(ptype, "retroharmonize_labelled_spss_survey")
   expect_equal(attr(ptype, "labels"), c(yes = 1, no = 0, inap = 9))
   expect_equal(attr(ptype, "na_values"), 9)
@@ -72,7 +71,7 @@ test_that("vec_c combines compatible labelled survey vectors", {
     id = "survey1",
     name_orig = "q1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(0, 1, 9),
     labels = c(yes = 1, no = 0, inap = 9),
@@ -81,9 +80,9 @@ test_that("vec_c combines compatible labelled survey vectors", {
     id = "survey2",
     name_orig = "q1"
   )
-  
+
   out <- vctrs::vec_c(x, y)
-  
+
   expect_s3_class(out, "retroharmonize_labelled_spss_survey")
   expect_equal(vctrs::vec_data(out), c(1, 0, 9, 0, 1, 9))
   expect_equal(attr(out, "labels"), c(yes = 1, no = 0, inap = 9))
@@ -97,13 +96,13 @@ test_that("vec_c errors when value labels differ", {
     labels = c(yes = 1, no = 0),
     id = "survey1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(1, 0),
     labels = c(ja = 1, nee = 0),
     id = "survey2"
   )
-  
+
   expect_error(vctrs::vec_c(x, y))
 })
 
@@ -114,13 +113,13 @@ test_that("vec_c errors when labelled numeric values differ", {
     labels = c(yes = 1, no = 0),
     id = "survey1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(1, 2),
     labels = c(yes = 1, no = 2),
     id = "survey2"
   )
-  
+
   expect_error(vctrs::vec_c(x, y))
 })
 
@@ -132,14 +131,14 @@ test_that("vec_c errors when na_values differ", {
     na_values = 9,
     id = "survey1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(1, 8),
     labels = c(yes = 1, inap = 9),
     na_values = 8,
     id = "survey2"
   )
-  
+
   expect_error(vctrs::vec_c(x, y))
 })
 
@@ -151,14 +150,14 @@ test_that("vec_c errors when na_range differs", {
     na_range = c(8, 9),
     id = "survey1"
   )
-  
+
   y <- labelled_spss_survey(
     x = c(1, 9),
     labels = c(yes = 1),
     na_range = c(9, 10),
     id = "survey2"
   )
-  
+
   expect_error(vctrs::vec_c(x, y))
 })
 
@@ -169,18 +168,18 @@ test_that("character cast only works for character labelled survey vectors", {
     labels = c(A = "a", B = "b"),
     id = "survey1"
   )
-  
+
   x_num <- labelled_spss_survey(
     x = c(1, 2),
     labels = c(A = 1, B = 2),
     id = "survey1"
   )
-  
+
   expect_equal(
     vctrs::vec_cast(x_chr, character()),
     c("a", "b")
   )
-  
+
   expect_error(
     vctrs::vec_cast(x_num, character())
   )
